5663e098e29501cf66dfed537c0ede7dc0130df6
CEI <admin@41.local>
2022-06-03 11:51:15 +0200

    Condicional en método attr

--- JavaScript/jQuery/metodoATTR/metodo.js
+++ JavaScript/jQuery/metodoATTR/metodo.js
@@ -1,8 +1,27 @@
-$(function(){
-    $('button').click(function(){
+$(function () {
+    $('button').click(function () {
+        // if ($('img').attr('src') == 'perro1.jpg') {
+        //     $('img').attr('src', 'perro2.jpg')
+        // } else if ($('img').attr('src') == 'perro2.jpg') {
+        //     $('img').attr('src', 'perro3.webp')
+        // } else {
+        //     $('img').attr('src', 'perro1.jpg')
+        // }
+
+        switch ($('img').attr('src')) {
+            case 'perro1.jpg':
+                $('img').attr('src', 'perro2.jpg')
+                break;
+            case 'perro2.jpg':
+                $('img').attr('src', 'perro3.webp')
+                break;
+            default:
+                $('img').attr('src', 'perro1.jpg')
+        }
+
+        // // console.log($('img').attr('alt'));
+        // $('img').attr('src', 'Perros2.webp')
+        // $('img').attr('alt', 'imagen de perro 2')
         // console.log($('img').attr('alt'));
-        $('img').attr('src', 'Perros2.webp')
-        $('img').attr('alt', 'imagen de perro 2')
-        console.log($('img').attr('alt'));
     })
 })

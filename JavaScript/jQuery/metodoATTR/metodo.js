49ec628a6e23351b2cd661955a23d36c983fb000
CEI <admin@41.local>
2022-06-03 11:36:12 +0200

    Método attr

---
+++ JavaScript/jQuery/metodoATTR/metodo.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('button').click(function(){
+        // console.log($('img').attr('alt'));
+        $('img').attr('src', 'Perros2.webp')
+        $('img').attr('alt', 'imagen de perro 2')
+        console.log($('img').attr('alt'));
+    })
+})

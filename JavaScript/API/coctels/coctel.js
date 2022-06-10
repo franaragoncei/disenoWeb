fbcca4a6c6e97dbef868e464f4db7094a0b06894
CEI <admin@41.local>
2022-06-10 09:43:17 +0200

    Message if drinks are null

--- JavaScript/API/coctels/coctel.js
+++ JavaScript/API/coctels/coctel.js
@@ -8,17 +8,22 @@ input.addEventListener('keypress', function () {
         .then(respuesta => respuesta.json())
         .then(datos => {
             console.log(datos);
-            datos.drinks.forEach(element => {
-                console.log(element);
-                contenedor.innerHTML += (
-                    '<div id="serie">' +
-                    '<div id="imagen">' +
-                    '<img src=' + element.strDrinkThumb + '></img>' +
-                    '</div>' +
-                    '<h3 id="nombre">' + element.strDrink + '</h3>' +
-                    '<p id="descripcion">' + element.strInstructions + '</p>' +
-                    '</div>'
-                )
-            })
+            if (datos.drinks == null) {
+                contenedor.innerHTML = '<p>Lo sentimos, no hay coincidencias con su búsqueda <span class="azul">' + input.value + '</span></p>'
+            } else {
+                datos.drinks.forEach(element => {
+                    console.log(element);
+                    contenedor.innerHTML += (
+                        '<div id="serie">' +
+                        '<div id="imagen">' +
+                        '<img src=' + element.strDrinkThumb + '></img>' +
+                        '</div>' +
+                        '<h3 id="nombre">' + element.strDrink + '</h3>' +
+                        '<p id="descripcion">' + element.strInstructions + '</p>' +
+                        '</div>'
+                    )
+                })
+            }
+
         })
 })

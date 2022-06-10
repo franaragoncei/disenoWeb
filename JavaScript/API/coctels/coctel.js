5a56ad5eef783fa6d7cfd706412a965764eeccd1
CEI <admin@41.local>
2022-06-10 09:21:06 +0200

    Coctel keypress event

--- JavaScript/API/coctels/coctel.js
+++ JavaScript/API/coctels/coctel.js
@@ -1,19 +1,24 @@
 var contenedor = document.getElementById('contenedor')
+var input = document.getElementById('coctel')
 
-fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-    .then(respuesta => respuesta.json())
-    .then(datos => {
-        console.log(datos);
-        datos.drinks.forEach(element => {
-            console.log(element);
-            contenedor.innerHTML += (
-                '<div id="serie">' +
-                '<div id="imagen">' +
-                '<img src=' + element.strDrinkThumb + '></img>' +
-                '</div>' +
-                '<h3 id="nombre">' + element.strDrink + '</h3>' +
-                '<p id="descripcion">' + element.strInstructions + '</p>' +
-                '</div>'
-            )
+input.addEventListener('keypress', function () {
+    console.log('hola');
+    contenedor.innerHTML = ''
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + input.value)
+        .then(respuesta => respuesta.json())
+        .then(datos => {
+            console.log(datos);
+            datos.drinks.forEach(element => {
+                console.log(element);
+                contenedor.innerHTML += (
+                    '<div id="serie">' +
+                    '<div id="imagen">' +
+                    '<img src=' + element.strDrinkThumb + '></img>' +
+                    '</div>' +
+                    '<h3 id="nombre">' + element.strDrink + '</h3>' +
+                    '<p id="descripcion">' + element.strInstructions + '</p>' +
+                    '</div>'
+                )
+            })
         })
-    })
+})
